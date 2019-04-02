40ec5eef7d54457ad063cc0096de5ef82bebcbdb
MikeKowalsky <mkowalewski83@gmail.com>
2019-04-02 05:43:04 +0200

    added Task model, form funcComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
+import { Task } from "./models/task";
+import { NewTaskForm } from "./components/NewTaskForm";
+
+interface State {
+  newTask: Task;
+  tasks: Task[];
+}
+
+class App extends Component<{}, State> {
+  state = {
+    newTask: {
+      id: 1,
+      name: ""
+    },
+    tasks: []
+  };
+
+  private handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      newTask: {
+        ...this.state.newTask,
+        name: event.target.value
+      }
+    });
+  };
+
+  private addTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    this.setState(prevState => ({
+      newTask: {
+        id: prevState.newTask.id + 1,
+        name: ""
+      },
+      tasks: [...prevState.tasks, prevState.newTask]
+    }));
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Learning TS!</h1>
+        <NewTaskForm
+          task={this.state.newTask}
+          onChange={this.handleTaskChange}
+          onAdd={this.addTask}
+        />
       </div>
     );
   }
